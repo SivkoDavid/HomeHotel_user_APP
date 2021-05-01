045a25e219b6cb829c94a0bb030040f795c3f62c
SivkoDavid <dyg87wgwiuegkj23>
2021-05-01 16:56:55 +0300

    Исправлено поведение диалога на экране гостя

--- app/src/main/java/VBllc/user/homehotel/Views/GuestView.kt
+++ app/src/main/java/VBllc/user/homehotel/Views/GuestView.kt
@@ -3,9 +3,11 @@ package VBllc.user.homehotel.Views
 import VBllc.user.homehotel.API.DataResponse.SettleResponse
 
 interface GuestView: BaseView {
-    fun showSettlement(data: SettleResponse.SettleData)
+    fun showSettlement(data: SettleResponse.SettleData, showDialog: Boolean = false)
 
     fun showNoGuestMode()
+
+    fun infoDialodHide()
 
     fun showCleaningFragment()
 }
