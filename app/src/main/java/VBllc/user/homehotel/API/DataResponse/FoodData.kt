23f535c893346dea5ab1d4da49fd59df7d7e98d4
SivkoDavid <dyg87wgwiuegkj23>
2021-05-05 16:25:22 +0300

    Карточка блюда

---
+++ app/src/main/java/VBllc/user/homehotel/API/DataResponse/FoodData.kt
@@ -0,0 +1,11 @@
+package VBllc.user.homehotel.API.DataResponse
+
+import android.content.ClipDescription
+
+data class FoodData(
+    val id: Int,
+    val name: String? = null,
+    val description: String? = null,
+    val imageLink: String? = null,
+    val price: Float? = null
+)
--- app/src/main/java/VBllc/user/homehotel/Main/ui/guest/Gastronomy/GastronomyFragment.kt
+++ app/src/main/java/VBllc/user/homehotel/Main/ui/guest/Gastronomy/GastronomyFragment.kt
@@ -1,20 +1,42 @@
 package VBllc.user.homehotel.Main.ui.guest.Gastronomy
 
+import VBllc.user.homehotel.API.DataResponse.FoodData
 import VBllc.user.homehotel.API.DataResponse.SettleResponse
-import VBllc.user.homehotel.Main.ui.guest.Hygiene.HygieneFragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import VBllc.user.homehotel.R
+import android.widget.BaseAdapter
+import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
 
 class GastronomyFragment : Fragment() {
 
     var settle: SettleResponse.SettleData? = null
+    private lateinit var grid: LinearLayout
 
-    fun initViews(root: View){
+    val testList = mutableListOf<FoodData>(
+        FoodData(1),
+        FoodData(1),
+        FoodData(1),
+        FoodData(1),
+        FoodData(1),
+        FoodData(1),
+        FoodData(1)
+    )
 
+    fun initViews(root: View, layoutInflater: LayoutInflater){
+        grid = root.findViewById(R.id.foodList)
+        grid.removeAllViewsInLayout()
+        grid.addView(LayoutInflater.from(requireContext()).inflate(R.layout.food_card, null))
+        grid.addView(LayoutInflater.from(requireContext()).inflate(R.layout.food_card, null))
+        grid.addView(LayoutInflater.from(requireContext()).inflate(R.layout.food_card, null))
+        grid.addView(LayoutInflater.from(requireContext()).inflate(R.layout.food_card, null))
+        grid.addView(LayoutInflater.from(requireContext()).inflate(R.layout.food_card, null))
+        grid.addView(LayoutInflater.from(requireContext()).inflate(R.layout.food_card, null))
+        val param = grid.layoutParams
     }
 
     override fun onCreateView(
@@ -22,7 +44,7 @@ class GastronomyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_gastronomy, container, false)
-        initViews(root)
+        initViews(root, inflater)
         return root
     }
 
@@ -32,5 +54,23 @@ class GastronomyFragment : Fragment() {
             GastronomyFragment().apply {
                 this.settle = settle
             }
+    }
+
+    inner class MGridAdapter(val data: MutableList<FoodData>): BaseAdapter() {
+
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val root = LayoutInflater.from(requireContext()).inflate(R.layout.food_card, null)
+            return root
+        }
+
+        override fun getItem(position: Int): Any = data[position]
+
+
+        override fun getItemId(position: Int): Long = position.toLong()
+
+
+        override fun getCount(): Int = data.count()
+
     }
 }
