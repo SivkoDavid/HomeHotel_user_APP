d5c7f837cd0b32111e5ad82b73c4ac58da5d299d
SivkoDavid <dyg87wgwiuegkj23>
2021-05-12 12:13:15 +0300

    Заявка на замену гигиенических средств

--- app/src/main/java/VBllc/user/homehotel/Main/ui/guest/Hygiene/HygieneFragment.kt
+++ app/src/main/java/VBllc/user/homehotel/Main/ui/guest/Hygiene/HygieneFragment.kt
@@ -7,23 +7,93 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import VBllc.user.homehotel.R
+import VBllc.user.homehotel.Views.OrderCleaningView
+import android.widget.Button
+import android.widget.TimePicker
+import android.widget.Toast
+import androidx.lifecycle.whenStarted
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 
-class HygieneFragment : Fragment() {
-    var settle: SettleResponse.SettleData? = null
+class HygieneFragment : Fragment(), OrderCleaningView {
+    var settleData: SettleResponse.SettleData? = null
+        set(value) {
+            field = value
+            presenter.settle = value
+        }
+
+    private lateinit var timeInput: TimePicker
+    private lateinit var sendBtn: Button
+    private val presenter = OrderHyegenePresenter(this)
+
+    fun initViews(root: View){
+        timeInput = root.findViewById(R.id.timeInput)
+        sendBtn = root.findViewById(R.id.orderButton)
+        sendBtn.setOnClickListener {
+            val dateNow = Calendar.getInstance().time
+            val date =
+                    "${dateNow.year}-${dateNow.month}-${dateNow.day} " +
+                            "${timeInput.hour}:${timeInput.minute}"
+            presenter.sendCleaningOrder(date)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hygiene, container, false)
+        val root = inflater.inflate(R.layout.fragment_hygiene, container, false)
+        initViews(root)
+        return root
     }
 
     companion object {
         @JvmStatic
         fun newInstance(settle: SettleResponse.SettleData) =
             HygieneFragment().apply {
-                this.settle = settle
+                settleData = settle
             }
+    }
+
+    override fun backToMenu() {
+        CoroutineScope(Dispatchers.Main).launch {
+            this@HygieneFragment.whenStarted {
+                fragmentManager?.popBackStack()
+            }
+        }
+    }
+
+    override fun showError(errorMessage: String, errorCode: Int) {
+        CoroutineScope(Dispatchers.Main).launch {
+            this@HygieneFragment.whenStarted {
+                Toast.makeText(requireContext(), "Ошибка: $errorMessage", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun showLoading() {
+        CoroutineScope(Dispatchers.Main).launch {
+            this@HygieneFragment.whenStarted {
+                Toast.makeText(requireContext(), "Загрузка", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun showNoNetwork() {
+        CoroutineScope(Dispatchers.Main).launch {
+            this@HygieneFragment.whenStarted {
+                Toast.makeText(requireContext(), "Нет подключения к интернету", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun showToast(text: String, length: Int) {
+        CoroutineScope(Dispatchers.Main).launch {
+            this@HygieneFragment.whenStarted {
+                Toast.makeText(requireContext(), text, length).show()
+            }
+        }
     }
 }
---
+++ app/src/main/java/VBllc/user/homehotel/Main/ui/guest/Hygiene/OrderHyegenePresenter.kt
@@ -0,0 +1,52 @@
+package VBllc.user.homehotel.Main.ui.guest.Hygiene
+
+import VBllc.user.homehotel.API.DataResponse.HotelServicesResponse
+import VBllc.user.homehotel.API.DataResponse.PartnersServicesResponse
+import VBllc.user.homehotel.API.DataResponse.SendOrderResponse
+import VBllc.user.homehotel.API.DataResponse.SettleResponse
+import VBllc.user.homehotel.DataLayer.Repositories.HotelServicesRepository
+import VBllc.user.homehotel.DataLayer.Repositories.HotelServicesRepositoryListener
+import VBllc.user.homehotel.Views.OrderCleaningView
+import android.widget.Toast
+
+class OrderHyegenePresenter(val view: OrderCleaningView) {
+    private val repository = HotelServicesRepository(RepositoryListener())
+
+    private val SEND_ORDER_CODE = 29
+
+    var settle: SettleResponse.SettleData? = null
+
+    fun sendCleaningOrder(time: String?){
+        repository.sendHugieneOrder(settle?.uid?:"", time, null, SEND_ORDER_CODE)
+    }
+
+    var lastServices: List<HotelServicesResponse.HotelServiceData>? = null
+
+    inner class RepositoryListener: HotelServicesRepositoryListener {
+        override fun onServicesResponse(response: HotelServicesResponse, code: Int) {}
+
+        override fun onPartnerServicesResponse(response: PartnersServicesResponse, code: Int) { }
+
+        override fun onSendOrderResponse(response: SendOrderResponse, code: Int) {
+            view.showToast("Заявка на услугу отправлена и вскоре будет обработана", Toast.LENGTH_LONG)
+            view.backToMenu()
+        }
+
+        override fun startRequest(name: String, code: Int) {
+            view.showLoading()
+        }
+
+        override fun noInternet(code: Int?) {
+            view.showNoNetwork()
+        }
+
+        override fun onErrors(errorMessages: List<String>, errorCode: Int, code: Int) {
+            var mess = ""
+            errorMessages.forEach{
+                mess += it+"\n"
+            }
+            mess = mess.substringBeforeLast('\n')
+            view.showError(mess, errorCode)
+        }
+    }
+}
